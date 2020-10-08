9866da33b296601596cf287b8d34595b50b35759
Soundwood <j.benhogan@gmail.com>
2020-10-08 11:50:24 -0500

    has_many syntax change

--- backend/app/models/offense_category.rb
+++ backend/app/models/offense_category.rb
@@ -1,4 +1,4 @@
 class OffenseCategory < ApplicationRecord
     has_many :profile_offenses
-    has_many :twitter_profiles, through: :profile_offenses
+    has_many :twitter_profiles, through: :profile_offenses, dependent: :destroy
 end
--- backend/app/models/twitter_profile.rb
+++ backend/app/models/twitter_profile.rb
@@ -1,4 +1,4 @@
 class TwitterProfile < ApplicationRecord
     has_many :profile_offenses
-    has_many :offense_categories, through: :profile_offenses
+    has_many :offense_categories, through: :profile_offenses, dependent: :destroy
 end

b425020225960d6cebb19ef739c294464d663296
Soundwood <j.benhogan@gmail.com>
2020-10-08 11:52:50 -0500

    fleshed out controllers

--- backend/app/controllers/offense_categories_controller.rb
+++ backend/app/controllers/offense_categories_controller.rb
@@ -1,2 +1,34 @@
 class OffenseCategoriesController < ApplicationController
+    before_action :set_offense, only: [:show, :update, :destroy]
+
+    def index
+        @offenses = OffenseCategory.all
+        render json: @offenses
+    end
+    def show
+        render json: @offense
+    end
+    def create
+        @offense = OffenseCategory.create!(offense_params)
+        render json: @offense
+    end
+    def update
+        @offense.update(offense_params)
+        head :no_content
+        # { head :no_content }?
+    end
+    def destroy
+        @offense.destroy
+        head :no_content
+    end
+
+    private
+
+    def offense_params
+        params.permit(:name)
+    end
+
+    def set_offense
+        @offense = OffenseCategory.find_by_id(params['id'])
+    end
 end
--- backend/app/controllers/twitter_profiles_controller.rb
+++ backend/app/controllers/twitter_profiles_controller.rb
@@ -1,12 +1,33 @@
 class TwitterProfilesController < ApplicationController
+    before_action :set_prof, only: [:show, :update, :destroy]
+
     def index
+        @profs = TwitterProfile.all
+        render json: @profs
     end
     def show
-    end
-    def new
+        render json: @prof
     end
     def create
+        @prof = TwitterProfile.create!(prof_params)
+        render json: @prof
     end
-    def edit
+    def update
+        @prof.update(prof_params)
+        head :no_content
+    end
+    def destroy
+        @prof.destroy
+        head :no_content
+    end
+
+    private
+
+    def prof_params
+        params.permit(:username, :user_id, :display_name)
+    end
+
+    def set_prof
+        @prof = TwitterProfile.find_by_id(params['id'])
     end
 end

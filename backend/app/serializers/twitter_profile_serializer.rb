4e120ec1a45f1bd044e843ec5781fe26397cfedb
Soundwood <j.benhogan@gmail.com>
2020-10-10 15:48:27 -0500

    changed attribute from protected word

--- backend/app/serializers/twitter_profile_serializer.rb
+++ backend/app/serializers/twitter_profile_serializer.rb
@@ -1,4 +1,4 @@
 class TwitterProfileSerializer < ActiveModel::Serializer
-  attributes :id, :username, :user_id, :display_name, :offender, :protected, :created_at
+  attributes :id, :username, :user_id, :display_name, :offender, :irreproachable, :created_at
   has_many :offense_categories, through: :profile_offenses
 end

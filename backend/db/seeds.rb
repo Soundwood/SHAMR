9131d4f06c0a8fad78cf51a35b2692b07bbf694f
Soundwood <j.benhogan@gmail.com>
2020-10-11 11:49:53 -0500

    added relations to seed file

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -7,28 +7,39 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-prof_a =TwitterProfile.create(username: 'realdonaldtrump', display_name: 'Donald J. Trump', user_id: 25073877, irreproachable: true) 
-TwitterProfile.create(username: 'barackobama', display_name: 'Barack Obama')
-TwitterProfile.create(username: 'katyperry', display_name: 'Katy Perry', offender: true)
-TwitterProfile.create(username: 'rihanna', display_name: 'Rihanna')
-TwitterProfile.create(username: 'justinbieber', display_name: 'Justin Bieber')
-TwitterProfile.create(username: 'taylorswift13', display_name: 'Taylor Swift', offender: true)
-TwitterProfile.create(username: 'TheEllenShow', display_name: 'Ellen DeGeneres')
-TwitterProfile.create(username: 'kimkardashian', display_name: 'Kim Kardashian')
-TwitterProfile.create(username: 'billgates', display_name: 'Bill Gates')
-TwitterProfile.create(username: 'elonmusk', display_name: 'Elon Musk', offender: true)
-TwitterProfile.create(username: 'britneyspears', display_name: 'Britney Spears')
-TwitterProfile.create(username: 'shakira', display_name: 'Shakira')
+prof_a = TwitterProfile.create(username: 'realdonaldtrump', display_name: 'Donald J. Trump', user_id: 25073877, irreproachable: true) 
+prof_b = TwitterProfile.create(username: 'barackobama', display_name: 'Barack Obama')
+prof_c = TwitterProfile.create(username: 'katyperry', display_name: 'Katy Perry', offender: true)
+prof_d = TwitterProfile.create(username: 'rihanna', display_name: 'Rihanna')
+prof_e = TwitterProfile.create(username: 'justinbieber', display_name: 'Justin Bieber')
+prof_f = TwitterProfile.create(username: 'taylorswift13', display_name: 'Taylor Swift', offender: true)
+prof_g = TwitterProfile.create(username: 'TheEllenShow', display_name: 'Ellen DeGeneres')
+prof_h = TwitterProfile.create(username: 'kimkardashian', display_name: 'Kim Kardashian')
+prof_i = TwitterProfile.create(username: 'billgates', display_name: 'Bill Gates')
+prof_j = TwitterProfile.create(username: 'elonmusk', display_name: 'Elon Musk', offender: true)
+prof_k = TwitterProfile.create(username: 'britneyspears', display_name: 'Britney Spears')
+prof_l = TwitterProfile.create(username: 'shakira', display_name: 'Shakira')
 
 
 off_a = OffenseCategory.create(name: 'none')
-OffenseCategory.create(name: 'use of customary units of measure')
-OffenseCategory.create(name: 'incorrect pronunciation of gif')
-OffenseCategory.create(name: 'does not include /s for sarcasm')
-OffenseCategory.create(name: 'mows their lawn before 8AM')
-OffenseCategory.create(name: 'slow-walker')
-OffenseCategory.create(name: 'not using turn indicator')
-OffenseCategory.create(name: 'loud chewer')
+off_b = OffenseCategory.create(name: 'use of customary units of measure')
+off_c = OffenseCategory.create(name: 'incorrect pronunciation of gif')
+off_d = OffenseCategory.create(name: 'does not include /s for sarcasm')
+off_e = OffenseCategory.create(name: 'mows their lawn before 8AM')
+off_f = OffenseCategory.create(name: 'slow-walker')
+off_g = OffenseCategory.create(name: 'not using turn indicator')
+off_h = OffenseCategory.create(name: 'loud chewer')
 
 # TwitterProfile.find_by_id(1).offense_categories << OffenseCategory.find_by_id(3)
-# sighting_a = profile_offenses.create(twitter_profile_id: prof_a, location: off_a)
+prof_a.offense_categories << off_a
+prof_b.offense_categories << off_c
+prof_c.offense_categories << off_d
+prof_d.offense_categories << off_b
+prof_e.offense_categories << off_f
+prof_f.offense_categories << off_g
+prof_g.offense_categories << off_c
+prof_h.offense_categories << off_a
+prof_i.offense_categories << off_h
+prof_j.offense_categories << off_d
+prof_k.offense_categories << off_a
+prof_l.offense_categories << off_b

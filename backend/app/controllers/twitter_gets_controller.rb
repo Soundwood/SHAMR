9153aace6b5adddd2dbaa4e64532a2c62c6498fa
Soundwood <j.benhogan@gmail.com>
2020-10-20 10:43:26 -0500

    update get request to include cursor variable

--- backend/app/controllers/twitter_gets_controller.rb
+++ backend/app/controllers/twitter_gets_controller.rb
@@ -1,11 +1,10 @@
 class TwitterGetsController < ApplicationController
     def show
         user = params['id']
-        twitter_v1_api_url = "https://api.twitter.com/1.1/friends/list.json?screen_name=#{user}&count=200"
-        # May need '&count=200' appended to the end of url string
+        twitter_v1_api_url = "https://api.twitter.com/1.1/friends/list.json?screen_name=#{user}&count=200&cursor=-1"
+        # '&cursor=-1' to atrt and then repeat until cursor = 0
         headers = {
             "Authorization" => "Bearer #{ENV["TWITTER_BEARER_TOKEN"]}"
-            # "Bearer" => ENV["TWITTER_BEARER_TOKEN"]
         }
         response = HTTParty.get(twitter_v1_api_url, headers: headers)
         render json: response

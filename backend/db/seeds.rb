b1d4bf9f45f510d890ec793dc3abfe1f7a798df6
Soundwood <j.benhogan@gmail.com>
2020-10-21 20:15:20 -0500

    update seed data with user_ids

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -8,17 +8,17 @@
 
 
 prof_a = TwitterProfile.create(username: 'realdonaldtrump', display_name: 'Donald J. Trump', user_id: 25073877, irreproachable: true) 
-prof_b = TwitterProfile.create(username: 'barackobama', display_name: 'Barack Obama')
-prof_c = TwitterProfile.create(username: 'katyperry', display_name: 'Katy Perry', offender: true)
-prof_d = TwitterProfile.create(username: 'rihanna', display_name: 'Rihanna')
-prof_e = TwitterProfile.create(username: 'justinbieber', display_name: 'Justin Bieber')
-prof_f = TwitterProfile.create(username: 'taylorswift13', display_name: 'Taylor Swift', offender: true)
-prof_g = TwitterProfile.create(username: 'TheEllenShow', display_name: 'Ellen DeGeneres')
-prof_h = TwitterProfile.create(username: 'kimkardashian', display_name: 'Kim Kardashian')
-prof_i = TwitterProfile.create(username: 'billgates', display_name: 'Bill Gates')
-prof_j = TwitterProfile.create(username: 'elonmusk', display_name: 'Elon Musk', offender: true)
-prof_k = TwitterProfile.create(username: 'britneyspears', display_name: 'Britney Spears')
-prof_l = TwitterProfile.create(username: 'shakira', display_name: 'Shakira')
+prof_b = TwitterProfile.create(username: 'barackobama', display_name: 'Barack Obama', user_id: 813286)
+prof_c = TwitterProfile.create(username: 'katyperry', display_name: 'Katy Perry', user_id: 21447363, offender: true)
+prof_d = TwitterProfile.create(username: 'rihanna', display_name: 'Rihanna', user_id: 79293791)
+prof_e = TwitterProfile.create(username: 'justinbieber', display_name: 'Justin Bieber', user_id: 27260086)
+prof_f = TwitterProfile.create(username: 'taylorswift13', display_name: 'Taylor Swift', user_id: 17919972, offender: true)
+prof_g = TwitterProfile.create(username: 'TheEllenShow', display_name: 'Ellen DeGeneres', user_id: 15846407)
+prof_h = TwitterProfile.create(username: 'kimkardashian', display_name: 'Kim Kardashian', user_id: 25365536)
+prof_i = TwitterProfile.create(username: 'billgates', display_name: 'Bill Gates', user_id: 50393960)
+prof_j = TwitterProfile.create(username: 'elonmusk', display_name: 'Elon Musk', user_id: 44196397, offender: true)
+prof_k = TwitterProfile.create(username: 'britneyspears', display_name: 'Britney Spears', user_id: 16409683)
+prof_l = TwitterProfile.create(username: 'shakira', display_name: 'Shakira', user_id: 44409004)
 
 
 off_a = OffenseCategory.create(name: 'none')

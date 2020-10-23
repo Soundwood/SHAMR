f61ee64c3c7f13be39877cacc11b7c9c64e3b4c0
Soundwood <j.benhogan@gmail.com>
2020-10-23 09:11:37 -0500

    formatting changes

--- backend/app/assets/javascripts/offenses.js
+++ backend/app/assets/javascripts/offenses.js
@@ -1,64 +1,65 @@
 class Offense {
-    static all = []
+  static all = []
 
-    constructor({ id, name, created_at}) {
-        this.id = id
-        this.name = name
-        this.created_at = created_at
-    }
-    render() {
-        Offense.all.push(this)
-        let offensesDiv = document.getElementById("Offenses")
-        let offenseCont = document.createElement('div')
-        offenseCont.setAttribute("id", `offense_container_for_${this.id}`)
-        let offenseH5 = document.createElement('h5')
-        let button = document.createElement('button')
-        button.innerText = `Remove from Offense List`
-        offenseH5.innerText = this.name + "  "
-        offensesDiv.appendChild(offenseCont)
-        offenseCont.appendChild(offenseH5)
-        offenseH5.appendChild(button)
-        button.addEventListener("click", (e) => {
-            // debugger
-            fetch(`http://localhost:3000/offense_categories/${this.id}`, {
-              method: "DELETE",
-            }).then(offenseCont.remove());
-        })
-    }
-    renderCheckbox() {
-        let off_cont = document.getElementById("off_opt_cont")
-        let checkbox = document.createElement('input')
-        let br = document.createElement("br")
-        checkbox.setAttribute("type", "checkbox")
-        checkbox.setAttribute("id", `checkbox_offense_id_${this.id}`)
-        checkbox.setAttribute("name", `${this.id}`)
-        let checkboxLabel = document.createElement('label')
-        checkboxLabel.setAttribute("for", `name_offense_${this.id}`)
-        checkboxLabel.innerText = `  ${this.name}`
-        off_cont.appendChild(checkbox)
-        off_cont.appendChild(checkboxLabel)
-        off_cont.appendChild(br)
-    }
-    createNew() {
-        fetch("http://localhost:3000/offense_categories", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                    name: this.name,
-                },
-            ),
-        }).then((resp) => resp.json())
-        .then((data) => {
-          if (data.errors) {
-            console.log(info.errors);
-          } else {
-            this.id = data.id;
-            document.querySelector('input[id=offense_input]').value = "";
-            const newOffense = new Offense(data)
-            newOffense.render()
-            newOffense.renderCheckbox()
-          }
-        })}
+  constructor({ id, name, created_at}) {
+      this.id = id
+      this.name = name
+      this.created_at = created_at
+  }
+  render() {
+      Offense.all.push(this)
+      let offensesDiv = document.getElementById("Offenses")
+      let offenseCont = document.createElement('div')
+      offenseCont.setAttribute("id", `offense_container_for_${this.id}`)
+      let offenseH5 = document.createElement('h5')
+      let button = document.createElement('button')
+      button.innerText = `Remove from Offense List`
+      offenseH5.innerText = this.name + "  "
+      offensesDiv.appendChild(offenseCont)
+      offenseCont.appendChild(offenseH5)
+      offenseH5.appendChild(button)
+      button.addEventListener("click", (e) => {
+          // debugger
+          fetch(`http://localhost:3000/offense_categories/${this.id}`, {
+            method: "DELETE",
+          }).then(offenseCont.remove());
+      })
+  }
+  renderCheckbox() {
+      let off_cont = document.getElementById("off_opt_cont")
+      let checkbox = document.createElement('input')
+      let br = document.createElement("br")
+      checkbox.setAttribute("type", "checkbox")
+      checkbox.setAttribute("id", `checkbox_offense_id_${this.id}`)
+      checkbox.setAttribute("name", `${this.id}`)
+      let checkboxLabel = document.createElement('label')
+      checkboxLabel.setAttribute("for", `name_offense_${this.id}`)
+      checkboxLabel.innerText = `  ${this.name}`
+      off_cont.appendChild(checkbox)
+      off_cont.appendChild(checkboxLabel)
+      off_cont.appendChild(br)
+  }
+  createNew() {
+      fetch("http://localhost:3000/offense_categories", {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+                  name: this.name,
+              },
+          ),
+      }).then((resp) => resp.json())
+      .then((data) => {
+        if (data.errors) {
+          console.log(info.errors);
+        } else {
+          this.id = data.id;
+          document.querySelector('input[id=offense_input]').value = "";
+          const newOffense = new Offense(data)
+          newOffense.render()
+          newOffense.renderCheckbox()
+        }
+      })
+  }
 }

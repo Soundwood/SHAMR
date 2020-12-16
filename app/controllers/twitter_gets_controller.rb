6a272a97d96cb81d15cfa8e88769cf51b96ff332
Soundwood <j.benhogan@gmail.com>
2020-12-16 09:47:30 -0600

    add shame functionality

--- app/controllers/twitter_gets_controller.rb
+++ app/controllers/twitter_gets_controller.rb
@@ -1,9 +1,11 @@
 class TwitterGetsController < ApplicationController
     def show
-        user = params['id']
-        twitter_v1_api_url = "https://api.twitter.com/1.1/friends/list.json?screen_name=#{user}&count=100"
+        user_and_cursor = params['id']
+        # twitter_v1_api_url = "https://api.twitter.com/1.1/friends/list.json?screen_name=#{user_and_cursor}&count=100" #old
+        twitter_v1_api_url = "https://api.twitter.com/1.1/friends/list.json?screen_name=#{user_and_cursor}&skip_status=true&include_user_entities=false" #new
         # '&cursor=-1' to atrt and then repeat until cursor = 0
         headers = {
+            "Content-type" => "application/json",
             "Authorization" => "Bearer #{ENV["TWITTER_BEARER_TOKEN"]}"
         }
         response = HTTParty.get(twitter_v1_api_url, headers: headers)
